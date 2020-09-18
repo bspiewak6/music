f074b20b15cbfba02ef773a0b1cf89a216e6f147
Peyton Schlafley <pschlafley0@gmail.com>
2020-09-18 17:16:26 -0400

    made the picture fit the width of the screen, and worked on javascript functions

--- js/script.js
+++ js/script.js
@@ -1,38 +1,45 @@
-var artistEl = document.querySelector("#artist-container");
+var artistContainerEl = document.querySelector("#artist-container");
+var artistSearch = document.querySelector("#artist-search");
+var userInput = document.querySelector("#icon-suffix");
+
 function getArtist() {
+  var artist = userInput.value.trim();
+
 var apiKey = "385243-TuneOut-LTR11AIV";
 fetch(
-    'https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=kanye west' + '&k=' + apiKey + '&limit=5'
+    'https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=' + artist + '&k=' + apiKey + '&limit=5'
   )
     .then(function(response) {
       return response.json();
     })
     .then(function(response) {
-      console.log(response.Similar.Results);
-      var artistName = response.Similar.Results[0].Name;
-      var artist = document.createElement("p");
-      artist.textContent = "Artist: " + artistName;
-      artistEl.appendChild(artist);
-      var artistName = response.Similar.Results[1].Name;
-      var artist = document.createElement("p");
-      artist.textContent = "Artist: " + artistName;
-      artistEl.appendChild(artist);
+      artistContainerEl.innerHTML= "";
+
+      var artistTag = document.createElement("p");
+      artistTag.innerHTML = "Artist: " + artist;
+      artistContainerEl.appendChild(artistTag);
     });
   };
-  getArtist();
-    var newsApiKey = "3f86fcdf-510e-4f3c-a66e-87ed087781ce";
-    var url =
-    "https://cors-anywhere.herokuapp.com/https://content.guardianapis.com/search?q=interviews&section=music&api-key=" + newsApiKey;
-    function getNews() {
-      // var req = new Request(url);
-      fetch (url)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(response) {
-        console.log(response.response.results[1].webTitle);
-      });
-    };
-    // getNews();
 
+  function formSubmitHandler(event) {
+    event.preventDefault();
+    getArtist();
+  };
+
+artistSearch.addEventListener("submit" , formSubmitHandler);
+
+    // var newsApiKey = "3f86fcdf-510e-4f3c-a66e-87ed087781ce";
+    // var url =
+    // "https://cors-anywhere.herokuapp.com/https://content.guardianapis.com/search?q=interviews&section=music&api-key=" + newsApiKey;
+    // function getNews() {
+    //   // var req = new Request(url);
+    //   fetch (url)
+    //   .then(function(response) {
+    //     return response.json();
+    //   })
+    //   .then(function(response) {
+    //     console.log(response.response.results[1].webTitle);
+    //   });
+    // };
+    // getNews();
 
