a3263490116d2f28c9189ad656800e32d2b3a5ff
Spiewak <bspiewak6@gmail.com>
2020-09-21 13:22:58 -0400

    updated youtube link for artist and added comments

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
+// global variables
 var artistContainerEl = document.querySelector("#artist-container");
 var artistSearch = document.querySelector("#artist-search");
 var userInput = document.querySelector("#icon_suffix");
 
+// function to get artist from user search
+// fetch call using tastedive API
 function getArtist() {
   var artist = userInput.value.trim();
 
@@ -14,12 +17,15 @@ fetch(
     })
     .then(function(response) {
       artistContainerEl.innerHTML= "";
-
       console.log(response);
 
+      // variable that pulls in user searched artist description 
       var artistInfoTeaser = response.Similar.Info[0].wTeaser;
+      
+      // variable that pulls in youtube link for user searched artist 
       var yUrl = response.Similar.Info[0].yUrl;
 
+      // dynamically created artist container 
       var artistCard = document.createElement("div");
       artistCard.classList = "card grey lighten-1";
       artistContainerEl.appendChild(artistCard);
@@ -33,8 +39,7 @@ fetch(
       cardTitle.innerHTML = "Artist:";
       cardContent.appendChild(cardTitle);
 
-      
-      var artistTag = document.createElement("p");
+      var artistTag = document.createElement("a");
       artistTag.classList = "red-text"
       artistTag.setAttribute("href", yUrl);
       artistTag.innerHTML = artist;
@@ -44,13 +49,10 @@ fetch(
       artistDescription.innerHTML = artistInfoTeaser;
       artistDescription.classList = "black-text";
       cardContent.appendChild(artistDescription);
-
-      // var artistVideo = document.createElement("link");
-      // artistVideo.setAttribute("href", yUrl);
-      // cardContent.appendChild(artistVideo);
-
-
+      
+      // dynamically created artist recommendation container
       var recContainer = document.createElement("div");
+      recContainer.classList = "rec-container";
       artistCard.appendChild(recContainer);
 
       var recTitle = document.createElement("span");
@@ -68,37 +70,42 @@ fetch(
       recList.appendChild(itemOne);
 
       var itemName = response.Similar.Results[1].Name;
-      var itemOne = document.createElement("li");
-      itemOne.innerHTML = itemName;
-      itemOne.classList = "red-text text-spacing"
-      recList.appendChild(itemOne);
+      var itemTwo = document.createElement("li");
+      itemTwo.innerHTML = itemName;
+      itemTwo.classList = "red-text text-spacing"
+      recList.appendChild(itemTwo); 
 
       var itemName = response.Similar.Results[2].Name;
-      var itemOne = document.createElement("li");
-      itemOne.innerHTML = itemName;
-      itemOne.classList = "red-text text-spacing"
-      recList.appendChild(itemOne);
+      var itemThree = document.createElement("li");
+      itemThree.innerHTML = itemName;
+      itemThree.classList = "red-text text-spacing"
+      recList.appendChild(itemThree);
 
       var itemName = response.Similar.Results[3].Name;
-      var itemOne = document.createElement("li");
-      itemOne.innerHTML = itemName;
-      itemOne.classList = "red-text text-spacing"
-      recList.appendChild(itemOne);
+      var itemFour = document.createElement("li");
+      itemFour.innerHTML = itemName;
+      itemFour.classList = "red-text text-spacing"
+      recList.appendChild(itemFour);
 
       var itemName = response.Similar.Results[4].Name;
-      var itemOne = document.createElement("li");
-      itemOne.innerHTML = itemName;
-      itemOne.classList = "red-text text-spacing"
-      recList.appendChild(itemOne);
+      var itemFive = document.createElement("li");
+      itemFive.innerHTML = itemName;
+      itemFive.classList = "red-text text-spacing"
+      recList.appendChild(itemFive);
     });
   };
 
+  // function to get artist from user search
   function formSubmitHandler(event) {
     event.preventDefault();
     getArtist();
   };
 
-artistSearch.addEventListener("submit" , formSubmitHandler);
+  // event listener added for user search button
+  artistSearch.addEventListener("submit", formSubmitHandler);
+
+
+
 
     // var newsApiKey = "3f86fcdf-510e-4f3c-a66e-87ed087781ce";
     // var url =
